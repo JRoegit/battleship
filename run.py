e62724ae7cf28056510028742d1a3944dd339dc0
Neimerai <124411766+Neimerai@users.noreply.github.com>
2023-12-05 14:11:46 -0500

    Updated run.py with new propositions

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
 
+
 # These two lines make sure a faster SAT solver is used.
 from nnf import config
 config.sat_backend = "kissat"
@@ -35,9 +36,60 @@ class FancyPropositions:
     def __repr__(self):
         return f"A.{self.data}"
 
+# Proposition for board cells
+@proposition(E)
+class BoardCell:
+    def __init__(self, player, x, y):
+        self.player = player  # Player 1 or 2
+        self.x = x  # X-coordinate
+        self.y = y  # Y-coordinate
+
+    def __repr__(self):
+        return f"BoardCell_Player{self.player}({self.x},{self.y})"
+
+# Proposition for ships
+@proposition(E)
+class Ship:
+    def __init__(self, player, ship_type, x, y):
+        self.player = player
+        self.ship_type = ship_type  # 'C', 'B', 'D', 'S', 'P'
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Ship{self.ship_type}_Player{self.player}({self.x},{self.y})"
+
+# Proposition for guesses
+@proposition(E)
+class Guess:
+    def __init__(self, player, guess_number, x, y):
+        self.player = player
+        self.guess_number = guess_number  # Guess turn number
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Guess{self.guess_number}_Player{self.player}({self.x},{self.y})"
+
+# Proposition for hits and misses
+@proposition(E)
+class HitMiss:
+    def __init__(self, player, guess_number, x, y, outcome):
+        self.player = player
+        self.guess_number = guess_number
+        self.x = x
+        self.y = y
+        self.outcome = outcome  # 'Hit' or 'Miss'
+
+    def __repr__(self):
+        return f"HitMiss{self.outcome}_Player{self.player}_Guess{self.guess_number}({self.x},{self.y})"
+
+
+
 # Call your variables whatever you want
 
-def map_init():
+
+def map_init(size):
     map = []
     for i in range(size):
         row = []
@@ -46,15 +98,15 @@ def map_init():
         map.append(row)
     return map
 
-#a = BasicPropositions("a")
-#b = BasicPropositions("b")   
-#c = BasicPropositions("c")
-#d = BasicPropositions("d")
-#e = BasicPropositions("e")
+a = BasicPropositions("a")
+b = BasicPropositions("b")
+c = BasicPropositions("c")
+d = BasicPropositions("d")
+e = BasicPropositions("e")
 # At least one of these will be true
-#x = FancyPropositions("x")
-#y = FancyPropositions("y")
-#z = FancyPropositions("z")
+x = FancyPropositions("x")
+y = FancyPropositions("y")
+z = FancyPropositions("z")
 
 
 # Build an example full theory for your setting and return it.
@@ -83,13 +135,13 @@ if __name__ == "__main__":
     T = T.compile()
     # After compilation (and only after), you can check some of the properties
     # of your model:
-    print("\nSatisfiable: %s" % T.satisfiable())
-    print("# Solutions: %d" % count_solutions(T))
-    print("   Solution: %s" % T.solve())
+    #print("\nSatisfiable: %s" % T.satisfiable())
+    #print("# Solutions: %d" % count_solutions(T))
+    #print("   Solution: %s" % T.solve())
 
-    print("\nVariable likelihoods:")
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
+    #print("\nVariable likelihoods:")
+    #for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
         # Ensure that you only send these functions NNF formulas
         # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
-    print()
+       # print(" %s: %.2f" % (vn, likelihood(T, v)))
+    #print()
